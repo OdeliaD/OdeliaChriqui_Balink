56783b25c1ec3c816af9595e0dec8c799bfc9454
OdeliaD <odeliachriqui@gmail.com>
2021-12-06 13:15:32 +0200

    odeliachriqui 06-12-2021

---
+++ Odelia/Odelia/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Odelia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult WeatherNextDays()
+        {
+            return View();
+        }
+        
+        public ActionResult WeatherByDay()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
